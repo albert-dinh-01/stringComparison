6d4708f38854f5d254e4d03fdcf2ba9fa6e012f5
Albert Dinh Personal <adinh.1867@gmail.com>
2022-05-13 12:55:10 -0600

    Tailwindcss modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {}
+		screens: {
+			xxl: { max: "1535px" },
+			// => @media (max-width: 1535px) { ... }
+
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+
+			lgMin: "1024px"
+			// => @media (min-width: 1024px) { ... }
+		},
+		fontSize: {
+			textLgMin: ["50px", "45px"],
+			textXLgMin: ["100px", "80px"],
+			textXSmMin: ["33px", "40px"]
+		}
 	},
 	plugins: []
 };
